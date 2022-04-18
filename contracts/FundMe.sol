d513a916ad10d20091f0c6d626d80fbfecdc30e9
Ben Brutocao <ben.brutocao@gmail.com>
2022-04-18 15:40:25 -0400

    First Ever!

---
+++ contracts/FundMe.sol
@@ -0,0 +1,66 @@
+//SPDX-License_Identifier: MIT
+
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+
+contract FundMe {
+    using SafeMathChainlink for uint256;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    address[] public funders;
+
+    address public owner;
+
+    AggregatorV3Interface public priceFeed;
+
+    constructor(address _priceFeed) public {
+        priceFeed = AggregatorV3Interface(_priceFeed);
+        owner = msg.sender;
+    }
+
+
+    function fund() public payable {
+        // we can set the price to whatever we want. Let's do $50
+        uint256 minimum = 60*(10**18);
+        require(getConversionRate(msg.value) >= minimum, "Fuck off Tiger");
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+        // need to know: Eth -> USD conversion rate
+
+    }
+
+    function getVersion() public view returns (uint256){
+        return priceFeed.version();
+    }
+    function getPrice() public view returns (uint256) {
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+          return uint256(answer);
+    }
+    function getConversionRate(uint256 ethAmt) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        return (ethAmt*ethPrice)/100000000;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function withdraw() payable onlyOwner public {
+        msg.sender.transfer(address(this).balance);
+        for (uint256 funderI = 0; funderI < funders.length; funderI++) {
+            address funder = funders[funderI];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        uint256 minimumUSD = 50 * 10**18;
+        uint256 price = getPrice();
+        uint256 precision = 1 * 10**18;
+        return (minimumUSD * precision) / price;
+    }
+}
